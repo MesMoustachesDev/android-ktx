2b401348e00676a1f5ad5792fc8ce41645d2a74a
t.francois <t.francois@useradgents.com>
2019-07-15 10:34:50 +0200

    DEV - adds retrofit call adapter for live data

---
+++ src/main/java/dev.mesmoustaches/network/LiveDataCallAdapter.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.mesmoustaches.network
+
+import androidx.lifecycle.LiveData
+import retrofit2.Call
+import retrofit2.CallAdapter
+import retrofit2.Callback
+import retrofit2.Response
+import java.lang.reflect.Type
+
+class LiveDataCallAdapter<R>(private val responseType: Type): CallAdapter<R, LiveData<ApiResponse<R>>> {
+    override fun adapt(call: Call<R>): LiveData<ApiResponse<R>> {
+        return object : LiveData<ApiResponse<R>>() {
+            private var isSuccess = false
+
+            override fun onActive() {
+                super.onActive()
+                if (!isSuccess) enqueue()
+            }
+
+            override fun onInactive() {
+                super.onInactive()
+                dequeue()
+            }
+
+            private fun dequeue() {
+                if (call.isExecuted) call.cancel()
+            }
+
+            private fun enqueue() {
+                call.enqueue(object : Callback<R> {
+                    override fun onFailure(call: Call<R>, t: Throwable) {
+                        postValue(ApiResponse.create(UNKNOWN_CODE, t))
+                    }
+
+                    override fun onResponse(call: Call<R>, response: Response<R>) {
+                        postValue(ApiResponse.create(response))
+                        isSuccess = true
+                    }
+                })
+            }
+        }
+    }
+
+    override fun responseType(): Type = responseType
+}
